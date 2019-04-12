ded34270a05232c7a698bd5d0338e51c322d203b
JackyLi-cityu <44828855+JackyLi-cityu@users.noreply.github.com>
2019-04-12 23:52:13 +0800

    Update admin.cpp

--- admin.cpp
+++ admin.cpp
@@ -1,34 +1,40 @@
 #include "Admin.h"
-#include<iostream>
-string Admin::getUserId() {
-	return _UserId;
-}
-
-string Admin::getpassword() {
-	return _password;
-}
-
-void Admin::setUserId(string UserId) {
-	_UserId = UserId;
-}
-void Admin::setpassword(string password) {
-	_password = password;
-}
-
-Admin::Admin() {
-	_UserId = "admin";
-	_password = "admin";
-}
-
-/*bool Admin::isAdminLogin(string UserId, string password) {
-	bool access = true;
-
-	access = ((_UserId == UserId) && (_password == password)) ? true : false;
-	system("cls");
-	return access;
-}*/
+#include "user.h"
+#include "camp_equipment.h"
+#include "loanControl.h"
+#include "filehandler.h"
+#include <iostream>
+using namespace std;
 
 void Admin::displayloanRecord() {
+	string line;
 	ifstream file;
 	file.open("user.txt");
+	string user;
+
+	if (file.is_open()) {
+		while (getline(file, user))
+			cout << user << endl;
+	}
+	cout << endl;
+	file.close;
+}
+void Admin::loanRecord(int loanDate, string itemCode, string itemName,
+	string NameOfBorrower, int returnDate, string status) {
+	this->_loanDate = loanDate;
+	this->_itemCode = itemCode;
+	this->_itemName = itemName;
+	this->_NameOfBorrower = NameOfBorrower;
+	this->_returnDate = returnDate;
+	this->_status = status;
+}
+
+void Admin::produceNewTextFile() {
+	ifstream file;
+	ifstream user("loanRecord.txt");
+	ifstream campEquipment("EquipementList.txt");
+	ofstream combined_file("loan.txt");
+	combined_file << user.rdbuf() << campEquipment.rdbuf();
+
+	file.close();
 }
